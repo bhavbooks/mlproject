1dbd5bfc07de8f07b1707907599ae3bd0b672737
Bhavesh Gupta <bhaveshkumar3832@gmail.com>
2025-09-06 19:14:05 +0530

    EDA and Problem Analysis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## End to end machine learning project
+## This project understand how the student's performance (test scores) is affected by other variables such as Gender, Ethnicity, Parental level of Education, Lunch and Test preparation course
+
--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -18,7 +17,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-#this will give the complete error message as well as the line number where the error occured , just like VS code does.
 
-
-
+## This is a custom exception class which will be used to handle exceptions throughout the project.
